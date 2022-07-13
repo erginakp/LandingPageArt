93e54bda1a4065c4eb0ff20f4677ad00803bfb58
Ergina Kamilia Putri <68270386+erginakp@users.noreply.github.com>
2022-07-13 16:40:44 +0700

    Add files via upload

---
+++ assets/js.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var nav = document.querySelector('.nav')
+    menuToggle.classList.toggle('active')
+    nav.classList.toggle('active')
+}
